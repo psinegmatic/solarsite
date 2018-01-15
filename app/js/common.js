8242ee9805184b038d82a281803dc86b9e36d0dc
psinegmatic <psinegmatic@gmail.com>
2018-01-15 17:53:41 +0300

    add burger menu

--- app/js/common.js
+++ app/js/common.js
@@ -2,4 +2,22 @@ $(document).on('click', 'a[href^="#"]', function (event) {
 	$('html, body').animate({
 		scrollTop: $($.attr(this, 'href')).offset().top
 	}, 500);
+	return false;
+});
+
+$(document).ready(function () {
+	$('.burger-toggle').click(function () {
+		$(this).toggleClass('burger-toggle--close');
+		$(this).siblings('.main-nav').toggleClass('main-nav--expand');
+	})
+});
+
+$(window).scroll(function() {
+	if ($(this).scrollTop() > 90){
+		$('.main-header').addClass("main-header--fixed");
+		$('.fixed-wrapper').addClass("fixed-wrapper--fixed");
+	} else {
+		$('.main-header').removeClass("main-header--fixed");
+		$('.fixed-wrapper').removeClass("fixed-wrapper--fixed");
+	}
 });
