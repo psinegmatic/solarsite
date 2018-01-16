ab5aaf6a0efba03398ae194ade2b9f8cad99ba6b
psinegmatic <psinegmatic@gmail.com>
2018-01-16 11:29:05 +0300

    add fixed mobile menu

--- app/js/common.js
+++ app/js/common.js
@@ -1,23 +1,44 @@
+function expandMenu() {
+	var burger = $('.burger-toggle');
+	burger.toggleClass('burger-toggle--close');
+	burger.siblings('.main-nav').toggleClass('main-nav--expand');
+}
+
+//Плавный скролл
 $(document).on('click', 'a[href^="#"]', function (event) {
 	$('html, body').animate({
 		scrollTop: $($.attr(this, 'href')).offset().top
 	}, 500);
+	expandMenu();
 	return false;
 });
 
+//Выпадающая навигация
 $(document).ready(function () {
 	$('.burger-toggle').click(function () {
-		$(this).toggleClass('burger-toggle--close');
-		$(this).siblings('.main-nav').toggleClass('main-nav--expand');
-	})
+		return expandMenu();
+	});
 });
 
-$(window).scroll(function() {
-	if ($(this).scrollTop() > 90){
+//Фиксированое меню
+$(window).scroll(function () {
+	if ($(this).scrollTop() > 90) {
 		$('.main-header').addClass("main-header--fixed");
 		$('.fixed-wrapper').addClass("fixed-wrapper--fixed");
 	} else {
 		$('.main-header').removeClass("main-header--fixed");
 		$('.fixed-wrapper').removeClass("fixed-wrapper--fixed");
 	}
+});
+
+var lastScrollTop = 0;
+$(window).scroll(function (event) {
+	var st = $(this).scrollTop();
+	console.log(st);
+	if (st > lastScrollTop) {
+		$('.fixed-wrapper').removeClass("fixed-wrapper--upsticky");
+	} else if (st > 300) {
+		$('.fixed-wrapper').addClass("fixed-wrapper--upsticky");
+	}
+	lastScrollTop = st;
 });
